2b09af2a42d4f35a38ee958abfea8a470af82cbf
goel-adii <adityagoel1011@gmail.com>
2023-08-08 19:14:14 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const _= require("lodash");
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port =process.env.PORT ||3000;
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
